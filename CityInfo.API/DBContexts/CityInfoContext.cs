11a92b4aa0349e0a17955550b5f9181b67b77e53
Georgiana Nitu <genitu@microsoft.com>
2024-07-31 20:52:28 +0300

    Seeded the database

--- CityInfo.API/DBContexts/CityInfoContext.cs
+++ CityInfo.API/DBContexts/CityInfoContext.cs
@@ -6,10 +6,71 @@ namespace CityInfo.API.DBContexts
     public class CityInfoContext : DbContext
     {
         public DbSet<City> Cities { get; set; }
-        public DbSet<PointOfInterest> PointOfInterests { get; set;}
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
 
         public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options) { }
 
+        // used to provide data for seeding the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasData(
+                new City("Slobozia")
+                {
+                    Id = 1,
+                    Description = "The one with Eiffel Tower"
+                },
+                new City("Pitesti")
+                {
+                    Id = 2,
+                    Description = "The one with Trivale Forest"
+                },
+                new City("Brasov")
+                {
+                    Id = 3,
+                    Description = "The one with the nice center"
+                }
+                );
+
+            modelBuilder.Entity<PointOfInterest>().HasData(
+                new PointOfInterest("Eiffel Tower")
+                {
+                    Id = 1, 
+                    CityId = 1,
+                    Description = "Built by a baron"
+                },
+                new PointOfInterest("City Center")
+                {
+                    Id = 2,
+                    CityId = 1,
+                    Description = "Where you can walk"
+                },
+                new PointOfInterest("Trivale Forest")
+                {
+                    Id = 3,
+                    CityId = 2,
+                    Description = "Walk in the forest"
+                },
+                new PointOfInterest("Parcul Lunca Arges")
+                {
+                    Id = 4,
+                    CityId = 2,
+                    Description = "Walk around and bike"
+                },
+                new PointOfInterest("Piata Sfatului")
+                {
+                    Id = 5,
+                    CityId = 3,
+                    Description = "The main attraction"
+                },
+                new PointOfInterest("Muntele Tampa")
+                {
+                    Id = 6,
+                    CityId = 3,
+                    Description = "You can hike"
+                }
+                ); 
+            base.OnModelCreating(modelBuilder);
+        }
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlite("connectionstring"); 
---
+++ CityInfo.API/Migrations/20240731174616_InitialDataSeed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The one with Eiffel Tower", "Slobozia" },
+                    { 2, "The one with Trivale Forest", "Pitesti" },
+                    { 3, "The one with the nice center", "Brasov" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Built by a baron", "Eiffel Tower" },
+                    { 2, 1, "Where you can walk", "City Center" },
+                    { 3, 2, "Walk in the forest", "Trivale Forest" },
+                    { 4, 2, "Walk around and bike", "Parcul Lunca Arges" },
+                    { 5, 3, "The main attraction", "Piata Sfatului" },
+                    { 6, 3, "You can hike", "Muntele Tampa" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
